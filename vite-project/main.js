1c8b8348f532ef851d75e36b1a5c4d02fc253cad
gliberov <124058342+gliberov@users.noreply.github.com>
2023-12-30 23:55:38 -0500

    ggd

--- vite-project/main.js
+++ vite-project/main.js
@@ -15,7 +15,7 @@ async function getData(URL){
   
 }
 getData(baseURL); */
-
+import {data} from "../vite-project/demo.js"
 const margin = { top:70, right: 30, bottom: 40, left: 80};
 const width = 1200 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
@@ -32,16 +32,30 @@ const svg = d3.select("#chart-container")
     .append("g")
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
-d3.json("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=TSLA&interval=5min&outputsize=full&apikey=754XM1A6MI6WI2K4").then(function (data) {
+d3.json("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=NVDA&interval=5min&outputsize=full&apikey=ET1R73GNIE7MF4EU"
+).then(function (data) {
     const dates = Object.keys(data['Time Series (5min)']);
-    const datesList = []
-    dates.forEach(date => datesList.push(date))
-    console.log(datesList.sort())
+    const fullList = []
     
-});
+    dates.forEach(date => {
+        const point = { 
+            date: date, 
+            open: parseFloat(data['Time Series (5min)'][date]["1. open"]),
+            high: parseFloat(data['Time Series (5min)'][date]["2. high"]),
+            low: parseFloat(data['Time Series (5min)'][date]["3. low"]),
+            close: parseFloat(data['Time Series (5min)'][date]["4. close"]),
+            volume: parseFloat(data['Time Series (5min)'][date]["5. volume"])}
+    fullList.push(point)
+    })
+    fullList.reverse()
+    const parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S")
+    fullList.forEach( d => {
+        d.date = parseDate(d.date);
+        d.open = +d.open;
+    });
 
-x.domain(d3.extent(data, d => d.date));
-y.domain([0, d3.max(data, d => d.value)]);
+x.domain(d3.extent(fullList, d => d.date));
+y.domain([0, d3.max(fullList, d => d.open)]);
 
 svg.append('g')
     .attr("transform", `translate(0, ${height})`)
@@ -54,11 +68,12 @@ svg.append('g')
 
 const line = d3.line()
     .x(d => x(d.date))
-    .y(d => y(d.value));
+    .y(d => y(d.open));
 
 svg.append('path')
-    .datum(dataset)
+    .datum(fullList)
     .attr("fill", "none")
     .attr("stroke", "steelblue")
     .attr("stroke-width", 1)
-    .attr("d", line);
+    .attr("d", line); 
+})
