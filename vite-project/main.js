95d74b09678710524057a6abba895cc9996cc713
gliberov <124058342+gliberov@users.noreply.github.com>
2023-12-31 17:21:39 -0500

    no

--- vite-project/main.js
+++ vite-project/main.js
@@ -16,6 +16,7 @@ async function getData(URL){
 }
 getData(baseURL); */
 import {data} from "../vite-project/demo.js"
+import '../css/style.css'
 const margin = { top:70, right: 30, bottom: 40, left: 80};
 const width = 1200 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
@@ -25,12 +26,20 @@ const x = d3.scaleTime()
 const y = d3.scaleLinear()
     .range([height,0]);
 
+const line = d3.line()
+    .x(d => x(d.date))
+    .y(d => y(d.open));
+
 const svg = d3.select("#chart-container")
     .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
     .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+const tooltip = d3.select("body")
+.append("div")
+.attr("class", "tooltip");
 
 d3.json("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=NVDA&interval=5min&outputsize=full&apikey=ET1R73GNIE7MF4EU"
 ).then(function (data) {
@@ -54,26 +63,133 @@ d3.json("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=
         d.open = +d.open;
     });
 
-x.domain(d3.extent(fullList, d => d.date));
-y.domain([0, d3.max(fullList, d => d.open)]);
+    x.domain(d3.extent(fullList, d => d.date));
+    y.domain(d3.extent(fullList, d => d.open));
 
-svg.append('g')
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x)
-    .ticks(d3.timeMonth.every(1))
-    .tickFormat(d3.timeFormat("%b %Y")));
+    svg.append('g')
+        .attr("transform", `translate(0,${height})`)
+        .style("font-size", "14px")
+        .call(d3.axisBottom(x)
+            .tickValues(x.ticks(d3.timeDay.every(5)))
+            .tickFormat(d3.timeFormat("%Y-%m-%d")))
+        .call(g => g.select(".domain").remove())
+        .selectAll(".tick line")
+        .style("stroke-opacity", 0)
+    svg.selectAll(".tick text")
+        .attr("fill", "#777");
 
-svg.append('g')
-    .call(d3.axisLeft(y))
+    svg.append("text")
+    .attr("transform","rotate(-90)")
+    .attr("x",0-(height/2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .style("font-size", "14px")
+    .style("fill", "#777")
+    .style("font-family", "sans-serif")
+    .text("Placeholder");
 
-const line = d3.line()
-    .x(d => x(d.date))
-    .y(d => y(d.open));
+    svg.append('g')
+    .style("font-size", "14px")
+    .call(d3.axisLeft(y)
+        .ticks((d3.max(fullList, d => d.open)))
+        .tickSize(0)
+        .tickPadding(10))
+    .call(g => g.select(".domain").remove())
+    .selectAll(".tick text")
+    .style("fill", "#777")
+    .style("visibility", (d, i, nodes) => {
+        if (i === 0) {
+            return "hidden";
+        } else {
+            return "visibile";
+        }
+    })
 
-svg.append('path')
-    .datum(fullList)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1)
-    .attr("d", line); 
+    svg.selectAll("xGrid")
+    .data(x.ticks().slice(1))
+    .join("line")
+    .attr("x1", d => x(d))
+    .attr("x2", d => x(d))
+    .attr("y1",0)
+    .attr("y2", height)
+    .attr("stroke", "#e0e0e0")
+    .attr("stroke-width", .5);
+
+    svg.selectAll("yGrid")
+        .data(y.ticks((d3.max(fullList, d=> d.open))).slice(1))
+        .join("line")
+        .attr("x1",0)
+        .attr("x2", width)
+        .attr("y1", d => y(d))
+        .attr("y2", d => y(d))
+        .attr("stroke", "#e0e0e0")
+        .attr("stroke-width", .5)
+
+    const path = svg.append('path')
+        .datum(fullList)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1)
+        .attr("d", line); 
+
+    const circle = svg.append("circle")
+        .attr("r",0)
+        .attr("fill", "steelblue")
+        .style("stroke", "white")
+        .attr("opacity", .70)
+        .style("pointer-events", "none");
+
+    const listeningRect = svg.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+
+    listeningRect.on("mousemove", function (event) {
+        const [xCoord] = d3.pointer(event,this)
+        const bisectDate = d3.bisector(d => d.date).left
+        const x0 = x.invert(xCoord);
+        const i = bisectDate(fullList, x0, 1)
+        const d0 = fullList[i - 1]
+        const d1 = fullList[i]
+        const d = x0 - d0.date > d1.date - x0 ? d1: d0
+        const xPos = x(d.date)
+        const yPos = y(d.open)
+
+        circle.attr("cx", xPos)
+            .attr("cy", yPos)
+
+
+        circle.transition()
+            .duration(50)
+            .attr('r', 5)
+
+        tooltip
+            .style("display", "block")
+            .style("left", `${xPos +100}px`)
+            .style("top", `${yPos+ 50}px`)
+            .html(`<strong>Date:</strong> ${d.date.toLocaleDateString()}<br><strong>Price:</strong> ${d.open}`)
+    })
+        listeningRect.on("mouseleave", function() {
+            circle.transition()
+                .duration(50)
+                .attr("r",0);
+            
+            tooltip.style("display","none")
+        })
+
+        svg.append("text")
+            .attr("class", "chart-title")
+            .attr("x", margin.left - 115)
+            .attr("y", margin.top - 100)
+            .style("font-size", "24px")
+            .style("font-weight", "bold")
+            .style("font-family", "sans-serif")
+            .text("PLACEHOLDER FOR TITLE")
+        
+            svg.append("text")
+            .attr("class", "source-credit")
+            .attr("x", width-1125)
+            .attr("y", height+ margin.bottom -3)
+            .style("font-size", "9px")
+            .style("font-family", "sans-serif")
+            .text("ty AlphaVantage for the datasets")
 })
